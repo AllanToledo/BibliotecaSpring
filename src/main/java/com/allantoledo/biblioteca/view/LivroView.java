b1d19a65fcb1f6123c565c90ecc89687ec62c3b1
Allan Toledo <a.llantoledo@hotmail.com>
2023-07-06 10:57:50 -0400

    ficdev

--- src/main/java/com/allantoledo/biblioteca/view/LivroView.java
+++ src/main/java/com/allantoledo/biblioteca/view/LivroView.java
@@ -1,10 +1,11 @@
 package com.allantoledo.biblioteca.view;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -19,15 +20,28 @@ public class LivroView {
 	
 	@GetMapping("/lista")
 	public ModelAndView listarLivros() {
-		var view = new ModelAndView("listarLivros");
+		var view = new ModelAndView("livro/listarLivros");
 		view.addObject("livros", livroService.getAllLivros());
 		return view;
 	}
 	
 	@GetMapping("/cadastrar")
 	public ModelAndView cadastrarLivros() {
-		var view = new ModelAndView("cadastrarLivros");
+		var view = new ModelAndView("livro/cadastrarLivros");
 		view.addObject("livro", new Livro());
+		return view;
+	}
+	
+	@GetMapping("/cadastrar/{id}")
+	public ModelAndView cadastrarLivros(@PathVariable Long id) {
+		var view = new ModelAndView("livro/cadastrarLivros");
+		Livro livro;
+		try {
+			livro = livroService.getLivro(id);
+		} catch (NotFoundException e) {
+			livro = new Livro();
+		}
+		view.addObject("livro", livro);
 		return view;
 	}
 	
@@ -35,7 +49,7 @@ public class LivroView {
 	public ModelAndView cadastrarLivros(Livro livro) {
 		livro.setDisponivel(true);
 		livroService.saveLivro(livro);
-		var view = new ModelAndView("cadastrarLivros");
+		var view = new ModelAndView("livro/cadastrarLivros");
 		view.addObject("mensagem", "Sucesso!");
 		view.addObject("livro", new Livro());
 		return view;
