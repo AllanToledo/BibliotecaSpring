d560464e87e64f96aaf68819c2faf3b6d3f4f99f
Allan Toledo <a.llantoledo@hotmail.com>
2023-07-14 10:56:30 -0400

    to entendendo nada

--- src/main/java/com/allantoledo/biblioteca/security/SpringSecurity.java
+++ src/main/java/com/allantoledo/biblioteca/security/SpringSecurity.java
@@ -1,5 +1,8 @@
 package com.allantoledo.biblioteca.security;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -9,6 +12,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -26,8 +30,8 @@ public class SpringSecurity {
 		return httpSecurity.csrf(AbstractHttpConfigurer::disable)
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.authorizeHttpRequests(authorize -> authorize
-						.requestMatchers(HttpMethod.POST, "/login").permitAll()
-						.requestMatchers(HttpMethod.GET, "/login").permitAll()
+						.requestMatchers(HttpMethod.POST, "/login/**").permitAll()
+						.requestMatchers(HttpMethod.GET, "/login/**").permitAll()
 						.requestMatchers(HttpMethod.GET, "/livro/**").hasRole("FUNCIONARIO")
 						.requestMatchers(HttpMethod.GET, "/emprestimo/**").hasRole("FUNCIONARIO")
 						.requestMatchers(HttpMethod.GET, "/usuario/**").hasRole("FUNCIONARIO")
@@ -35,8 +39,7 @@ public class SpringSecurity {
 						.requestMatchers(HttpMethod.DELETE, "/biblioteca/**").hasRole("ADMIN")
 						.anyRequest().authenticated())
 				.formLogin((formLogin) -> formLogin
-						.loginPage("/login")
-						.failureUrl("/login-error"))
+					      .loginPage("/login"))
 				.build();
 	}
 
@@ -45,20 +48,23 @@ public class SpringSecurity {
 			throws Exception {
 		return authenticationConfiguration.getAuthenticationManager();
 	}
-
+	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-
+	
 	@Bean
 	public UserDetailsService userDetailsService() {
-		UserDetails userDetails = User.withDefaultPasswordEncoder()
-				.username("user")
-				.password("password")
-				.roles("ADMIN")
-				.build();
-		return new InMemoryUserDetailsManager(userDetails);
+		  var userDetailsService = new InMemoryUserDetailsManager();
+
+		  var user = User.withUsername("user")
+		          .password(this.passwordEncoder().encode("123456"))
+		          .authorities(List.of(new SimpleGrantedAuthority("ADMIN"), new SimpleGrantedAuthority("FUNCIONARIO"), new SimpleGrantedAuthority("USER")))
+		          .build();
+
+		  userDetailsService.createUser(user); 
+		  return userDetailsService;
 	}
 
 }
--- src/main/java/com/allantoledo/biblioteca/view/LoginView.java
+++ src/main/java/com/allantoledo/biblioteca/view/LoginView.java
@@ -1,22 +1,53 @@
 package com.allantoledo.biblioteca.view;
 
 import com.allantoledo.biblioteca.security.RegisterDTO;
+import com.allantoledo.biblioteca.security.UserRole;
+
+import jakarta.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/login")
 public class LoginView {
     @GetMapping
-    public String login() {
-        return "login";
+    public ModelAndView login() {
+    	var view = new ModelAndView("login");
+        view.addObject("register", new RegisterDTO("", "", UserRole.USER));
+    	return view;
     }
-
-    @PostMapping
-    public String logar(RegisterDTO registerDTO){
-
-        return "redirect:/";
+    
+    @GetMapping("/erro")
+    public ModelAndView loginErro() {
+    	var view = new ModelAndView("login");
+        view.addObject("erro", "Usuário ou senha inccorretos.");
+        view.addObject("register", new RegisterDTO("", "", UserRole.USER));
+    	return view;
     }
+    
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    
+    @PostMapping("/try")
+    public String loginTry(RegisterDTO data){
+    	System.out.println(data);
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
+        var auth = this.authenticationManager.authenticate(usernamePassword);
+        System.out.println(usernamePassword);
+        return "redirect:/livro";
+    }  
 }
